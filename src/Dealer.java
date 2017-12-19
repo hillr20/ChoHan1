33dc54bed5a41cd33be439f6385eaa03db955a6f
hillr <hillr@LR064W92-1718.ellis.int>
2017-12-18 22:36:28 -0500

    Okay, then.

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,4 +1,7 @@
-
+import javax.swing.JOptionPane;
 public class Dealer {
+	//attributes
+	private int die1Value;
+	private int die2Value;
 
 }
--- src/Die.java
+++ src/Die.java
@@ -1,4 +1,18 @@
-
+import java.util.Random;
 public class Die {
+	//attributes
+	private int sides;
+	private int value;
+	
+	public Die (int numSides) {
+		sides = numSides;
+		roll();
+	}
+	public void roll(){ 
+	      Random randroll = new Random();
+	  
+	      value = randroll.nextInt(sides) + 1;
+	   }
+	
 
 }
